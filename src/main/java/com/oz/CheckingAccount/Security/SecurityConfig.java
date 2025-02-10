46901c6c4ebed9794161f7efbf675c4feb469b99
Osmani-Josue Hernandez <osmani.j.hernandez@gmail.com>
2025-02-09 23:24:01 -0500

    add security filter chain, user details manager and password encoder

--- src/main/java/com/oz/CheckingAccount/Security/SecurityConfig.java
+++ src/main/java/com/oz/CheckingAccount/Security/SecurityConfig.java
@@ -2,8 +2,15 @@ package com.oz.CheckingAccount.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,11 +18,31 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                    .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(HttpMethod.POST, "/accounts/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/accounts/**").hasRole("USER")
+
+                    .anyRequest().denyAll())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(CsrfConfigurer::disable);
 
         return http.build();
     }
 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("mary")
+                                .password(passwordEncoder.encode("123987"))
+                                .roles("USER").build();
+
+        UserDetails oz = User.withUsername("oz")
+                .password(passwordEncoder.encode("abc123"))
+                .roles("USER").build();
+        return new InMemoryUserDetailsManager(user, oz);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
